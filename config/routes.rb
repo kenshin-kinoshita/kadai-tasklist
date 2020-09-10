ffc0c761be2495edacff7fc2620c4ba0a72f2c04
kenshin.kinoshita <temiyage6240@gmail.com>
2020-09-10 12:34:53 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'toppages#index'
-    
+  root to: 'tasks#index'
+  
+  get 'toppage', to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:new, :create]
   resources :tasks
 end
