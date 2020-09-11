7974340d82edcd2ae644126c502a5d30d7ff0061
kenshin.kinoshita <temiyage6240@gmail.com>
2020-09-11 01:40:15 +0000

    toppage fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   
-  get 'toppage', to: 'toppages#index'
+  get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#create'
+  get 'login', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:new, :create]
